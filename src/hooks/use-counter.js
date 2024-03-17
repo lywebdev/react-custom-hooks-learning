d030c8b61c9fff4b1762b4bbd54636c75f95c248
Kulik Leonid <lyonyauei@gmail.com>
2024-03-17 21:03:18 +0100

    RCHL-168: adding a parameter to a custom hook

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,15 +1,19 @@
 import {useEffect, useState} from "react";
 
-const useCounter = () => {
+const useCounter = (isPositive = true) => {
     const [counter, setCounter] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCounter((prevCounter) => prevCounter + 1);
+            if (isPositive) {
+                setCounter((prevCounter) => prevCounter + 1);
+            } else {
+                setCounter((prevCounter) => prevCounter - 1);
+            }
         }, 1000);
 
         return () => clearInterval(interval);
-    }, []);
+    }, [isPositive]);
 
     return counter;
 };
