87f5a8ced8bbbfe0dfd85ab0e9de0cdc7c39af7f
Kulik Leonid <lyonyauei@gmail.com>
2024-03-17 20:56:12 +0100

    RCHL-167

--- src/components/PositiveCounter.js
+++ src/components/PositiveCounter.js
@@ -1,17 +1,20 @@
 import { useState, useEffect } from "react";
 
 import Card from "./Card";
+import useCounter from "../hooks/use-counter";
 
 const PositiveCounter = () => {
-  const [counter, setCounter] = useState(0);
+  const counter = useCounter();
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
+  // const [counter, setCounter] = useState(0);
+  //
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCounter((prevCounter) => prevCounter + 1);
+  //   }, 1000);
+  //
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return <Card>{counter}</Card>;
 };
